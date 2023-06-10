0ab6f1ec010dff342ba41f4891aef03adf06684b
MissterMan <putrayogi4545@gmail.com>
2023-06-10 17:06:22 +0700

    Fix minor bug

--- src/config/database.js
+++ src/config/database.js
@@ -1,43 +1,21 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
-if (process.env.NODE_ENV === "development") {
-  const db_connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "db_donor",
-  });
-
-  db_connection.connect((error) => {
-    if (error) {
-      console.error(`Error connecting to MySQL: ${error}`);
-      return;
-    }
-
-    console.log("Connected to MySQL database");
-  });
-
-  module.exports = db_connection;
-}
-
 // PRODUCTION ONLY (DONT FORGET TO START THE MACHINE)
-if (process.env.NODE_ENV === "production") {
-  const db_connection = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-  });
+const db_connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+});
 
-  db_connection.connect((error) => {
-    if (error) {
-      console.error(`Error connecting to MySQL: ${error}`);
-      return;
-    }
+db_connection.connect((error) => {
+  if (error) {
+    console.error(`Error connecting to MySQL: ${error}`);
+    return;
+  }
 
-    console.log("Connected to MySQL database");
-  });
+  console.log("Connected to MySQL database");
+});
 
-  module.exports = db_connection;
-}
+module.exports = db_connection;
