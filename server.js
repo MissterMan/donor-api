e5a15fe95d4ecaefd523c9ea851958f8baf29707
MissterMan <putrayogi4545@gmail.com>
2023-06-10 13:10:20 +0700

    Minor fixing

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const admin = require("firebase-admin");
-const routes = require("./routes/route");
+const routes = require("./src/routes/route");
 
 // Initialize Firebase Admin SDK
-const serviceAccount = require("./config/ServiceAccountKey.json");
+const serviceAccount = require("./src/config/ServiceAccountKey.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 const app = express();
 const port = 3000;
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.use(express.json());
 app.use("/", routes);
--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,12 @@
 const mysql = require("mysql");
+require("dotenv").config();
 
+// PRODUCTION ONLY (DONT FORGET TO START THE MACHINE)
 const db_connection = mysql.createConnection({
-  host: "{CHANGE TO THE HOST NAME}",
-  user: "{USERNAME}",
-  password: "{PASSWORD}",
-  database: "{DATABASE NAME}",
+  host: process.env.HOST || "localhost",
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
 });
 
 db_connection.connect((error) => {
@@ -12,6 +14,7 @@ db_connection.connect((error) => {
     console.error(`Error connecting to MySQL: ${error}`);
     return;
   }
+
   console.log("Connected to MySQL database");
 });
 
